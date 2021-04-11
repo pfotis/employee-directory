87a91db9d97e5dd4331296845b2f81c55063ba44
pfotis <69961508+pfotis@users.noreply.github.com>
2021-04-11 23:41:00 +1000

    create the employee card and the main

--- src/components/EmployeeCard/index.js
+++ src/components/EmployeeCard/index.js
@@ -1,24 +1,46 @@
 import React from "react";
-import "./style.css";
+import { MDBDataTable } from "mdbreact";
+import "./employeecard.css";
 
 function EmployeeCard(props) {
+  const data = {
+    columns: [
+      {
+        label: "Image",
+        field: "image",
+        sort: "asc",
+        width: 500,
+      },
+      {
+        label: "Name",
+        field: "name",
+        sort: "asc",
+        width: 500,
+      },
+      {
+        label: "Email",
+        field: "email",
+        sort: "asc",
+        width: 500,
+      },
+      {
+        label: "Phone",
+        field: "phone",
+        sort: "asc",
+        width: 300,
+      },
+      {
+        label: "City",
+        field: "city",
+        sort: "asc",
+        width: 250,
+      }
+    ],
+    rows:props.employees
+  };
     return (
-      <>
-          <div className="content">
-            <ul className="card">
-              <li>
-                <img src={props.img}></img>
-              </li>
-              <li>
-                {props.first}
-              </li>
-              <li>
-                {props.last}
-              </li>
-            </ul>
-          </div>
-        </>
-      );
+      <MDBDataTable sorting={true} striped bordered data={data} />
+    );
 }
 
 export default EmployeeCard;
--- src/components/Main/index.js
+++ src/components/Main/index.js
@@ -1,8 +1,33 @@
-import React from "react";
+import React, { Component } from "react";
+import EmployeeCard from "../EmployeeCard/index";
+import API from "../../utils/API";
 
+class Main extends Component {
+  state = {
+    results: []
+  };
 
-function Main (){
-    return ();
-};
+  componentDidMount() {
+    this.searchEmployees();
+  }
+
+  searchEmployees = () => {
+    API.search()
+      .then(res => {
+        this.setState({ results: res.data.results });
+      })
+      .catch(err => console.log(err));
+  };
+
+  render() {
+    //console.log(this.state.results);
+    return (
+      <div>
+        <h2>Employee Table</h2>
+        <EmployeeCard employees={this.state.results} />
+      </div>
+    );
+  }
+}
 
 export default Main;
